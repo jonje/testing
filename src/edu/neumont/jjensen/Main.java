a493d9d97436f85b581018d140f27e5981f6e7fc
Jonathan Jensen <haiyun211@gmail.com>
2015-10-27 20:34:54 -0600

    Hello World Complete

--- src/edu/neumont/jjensen/Main.java
+++ src/edu/neumont/jjensen/Main.java
@@ -6,5 +6,6 @@ package edu.neumont.jjensen;
 public class Main {
     public static void main(String[] args) {
         // This is to test tags.
+        System.out.println("Hello World!");
     }
 }
